2981fba1a4d3ebc9de349bd10b152f0ac0c4ca0e
ronaldsalili1 <ronald@beautitag.com>
2024-04-21 11:23:59 +0800

    Config fix for production

--- src/config/environments/production.js
+++ src/config/environments/production.js
@@ -2,15 +2,23 @@ export default {
     api: {
         domain: 'mnhs.pitcompanion.com',
         port: 3000,
+        redis: {
+            host: '127.0.0.1',
+            port: '6379',
+            db: 0,
+        },
     },
     admin: {
-        host: 'https://mnhs.pitcompanion.com/admin',
+        host: 'https://mnhs.pitcompanion.com',
+        prefix: '/admin',
     },
     teacher: {
-        host: 'https://mnhs.pitcompanion.com/teacher',
+        host: 'https://mnhs.pitcompanion.com',
+        prefix: '/teacher',
     },
     student: {
-        host: 'https://mnhs.pitcompanion.com/student',
+        host: 'https://mnhs.pitcompanion.com',
+        prefix: '/student',
     },
     worker: {
         domain: '127.0.0.1',
